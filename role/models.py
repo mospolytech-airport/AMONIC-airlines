2d4b17313668487e04c3f50313fe1935eda7e137
Mikhail Sergeev <sergeevm100@gmail.com>
2023-09-19 21:03:06 +0300

    role foreign key

--- role/models.py
+++ role/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from auth.models import User
 
 
 class Role(models.Model):
     title = models.CharField(max_length=200)
+    role_id = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
