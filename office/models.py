3799717b62e761aaf43b5b72c9018c8596aadcaf
emilkaraev <EKaraev@alfabank.ru>
2023-09-19 22:37:32 +0300

    feat: added country_id to office model

--- office/models.py
+++ office/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+from country.models import Country
+
 class Office(models.Model):
     title = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
