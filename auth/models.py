64f097274d8e3cf7fbda31cc08c689115195cfda
Mikhail Sergeev <sergeevm100@gmail.com>
2023-09-19 20:57:09 +0300

    user model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='E-mail', max_length=500, unique=True)
+    first_name = models.CharField(verbose_name='Имя', max_length=50)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=150)
+    birthday = models.DateField(label='День рождения')
+    is_active = models.BooleanField(label='Актвность')
+    # password по умолчанию в AbstractBaseUser
+    # last_login по умолчанию в AbstractBaseUser
+    # is_superuser по умолчанию в PermissionsMixin
