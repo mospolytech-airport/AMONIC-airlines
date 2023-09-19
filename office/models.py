8aaa111c6a4fc870e13f3bad56ec460b7aba6839
emilkaraev <EKaraev@alfabank.ru>
2023-09-19 22:27:30 +0300

    feat: added office model

--- office/models.py
+++ office/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Office(models.Model):
+    title = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
