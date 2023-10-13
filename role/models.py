18d72051106a591e69ac18f69a76aa307a083b1a
Mikhail Sergeev <sergeevm100@gmail.com>
2023-10-13 20:28:08 +0300

    new fixtures

--- role/models.py
+++ role/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from authentication.models import User
 
 
 class Role(models.Model):
     title = models.CharField(max_length=200)
-    role_id = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
