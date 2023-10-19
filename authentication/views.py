75e57265d97e4d18ecc90061cbe519cb33fbc4a1
emilkaraev <EKaraev@alfabank.ru>
2023-10-19 11:01:23 +0300

    fix: fixed role fixture and auth views

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import ValidationError, NotFound, AuthenticationFailed
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from authentication.models import User
@@ -59,7 +59,7 @@ class UserViewSet(ModelViewSet):
         return response
 
     @action(methods=['GET'], detail=False, permission_classes=[IsAuthenticated], url_path='me')
-    def get_user(self, request):
+    def me(self, request):
         user = request.user
         data = self.serializer_class(user).data
 
@@ -85,3 +85,45 @@ class UserViewSet(ModelViewSet):
         user.delete()
         return Response({'message': 'user delete'})
     
+    @action(methods=['PATCH'], detail=False, permission_classes=[IsAdminUser], url_path='edit')
+    def edit_user(self, request):
+        email = request.data.get('email')
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        office = request.data.get('office')
+        role = request.data.get('role')
+
+        if email is None:
+            raise ValidationError({ 'error': 'email must not be empty' })
+        
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise NotFound({ 'error': 'user with this id was not found'})     
+
+        if first_name: setattr(user, 'first_name', first_name)
+        if last_name: setattr(user, 'last_name', last_name)
+        if office: setattr(user, 'office', office)
+        if role: setattr(user, 'role', role)
+
+        user.save()
+
+        data = self.serializer_class(user).data
+
+        return Response(data)
+
+    @action(methods=['GET'], detail=False, permission_classes=[IsAdminUser], url_path='users')
+    def get_users(self, request):
+        users = User.objects.all()
+
+        data = UserSerializer(users, many=True).data
+
+        return Response(data)
+    
+    @action(methods=['GET'], detail=False, permission_classes=[IsAdminUser], url_path=r'user/(?P<id>.*)')
+    def get_user(self, request, id):
+        user = User.objects.get(id=id)
+
+        data = UserSerializer(user).data
+
+        return Response(data)
