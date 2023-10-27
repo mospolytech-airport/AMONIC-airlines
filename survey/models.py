9aa4275bf013aaaaae224af400bf6adb4ee365dd
AndrewwKovv <kovshov.K@yandex.ru>
2023-10-27 15:57:24 +0300

    fixed fixtures into survey

--- survey/models.py
+++ survey/models.py
@@ -1,14 +1,20 @@
+from datetime import datetime
 from django.db import models
 
 
 class Survey(models.Model):
-    mounth = models.DateField(verbose_name='Месяц опроса', blank=True, null=True)
+    month = models.DateField(verbose_name='Месяц опроса', blank=True, null=True)
     departure = models.CharField(max_length=200)
     arrival = models.CharField(max_length=200)
-    age = models.IntegerField()
+    age = models.CharField(max_length=200, blank=True, null=True)
     gender = models.CharField(max_length=200)
     cabintype = models.CharField(max_length=200)
-    q1 = models.IntegerField()
-    q2 = models.IntegerField()
-    q3 = models.IntegerField()
-    q4 = models.IntegerField()
+    q1 = models.CharField(max_length=200,blank=True, null=True)
+    q2 = models.CharField(max_length=200,blank=True, null=True)
+    q3 = models.CharField(max_length=200,blank=True, null=True)
+    q4 = models.CharField(max_length=200,blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.month_str:  # Предполагается, что у вас есть поле month_str для хранения строки даты
+            self.month = datetime.strptime(self.month_str, '%d.%m.%Y').date()
+        super(Survey, self).save(*args, **kwargs)
