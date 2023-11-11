90c92dd8f90aabb40a9ab5c5e9e5cdd72b57b278
Mikhail Sergeev <sergeevm100@gmail.com>
2023-11-11 17:37:44 +0300

    import-export

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,3 +1,17 @@
+from import_export import resources, formats
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export.formats import base_formats
 
-# Register your models here.
+from role.models import Role
+from schedules.models import Schedule
+
+class ScheduleResource(resources.ModelResource):
+    class Meta:
+        model = Schedule
+
+class ScheduleAdmin(ImportExportModelAdmin):
+    resource_classes = [ScheduleResource]
+    formats = [base_formats.XLSX]
+
+admin.site.register(Schedule, ScheduleAdmin)
